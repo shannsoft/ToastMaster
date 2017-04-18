aaeba28f8409440e2836b580e435c2e0957e4b65
Santosh <santoshmajhi99@gmail.com>
2017-04-18 12:51:03 +0530

    minor changes for google map

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,6 +1,6 @@
 app.factory("Config", function($rootScope) {
   var gApi = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
-  var gApiKey = "AIzaSyAyPOEsucm0X1kbw1HVmE-fEOa2ArhupZg";
+  var gApiKey = "AIzaSyDXNf0gBHsSBF-tRQoDbVM9TJBso4wD9_o";
   return{
   	getLocationUrl : function(param){
   		var url = gApi+param+"&key="+gApiKey;
